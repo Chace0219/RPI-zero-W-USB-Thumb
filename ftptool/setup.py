fbf1b5cadb27be277d1d4f668b65449f6b4a8ebb
DESKTOP-U0UGAT3\Embedded <2435575291@qq.com>
2017-12-07 08:08:59 -0800

    no message

--- ftptool/setup.py
+++ ftptool/setup.py
@@ -7,7 +7,7 @@ readme_text = open(readme_fname).read()
 setup(name="ftptool", version="1.1.0",
       url="",
       description="Higher-level interface to ftplib",
-      author="Jinzhouyun",
+      author="",
       author_email="2435575291@qq.com",
       requires=["six"],
       long_description=readme_text,
